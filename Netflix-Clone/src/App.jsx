73a0dc6eb4bd43e427c2bdae0e046a2b011d81df
Eliyas daba <eladaba2022@gmail.com>
2024-10-04 21:39:37 +0300

    header and footer

--- Netflix-Clone/src/App.jsx
+++ Netflix-Clone/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello World</h1>
+      <Header/>
+      <Footer/>
     </>
   )
 }
