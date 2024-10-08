2166199497e3eece272f234625b55af3fbf6cfa0
Eliyas daba <eladaba2022@gmail.com>
2024-10-08 23:12:22 +0300

    final commit

--- Netflix-Clone/src/components/Banner/Banner.jsx
+++ Netflix-Clone/src/components/Banner/Banner.jsx
@@ -36,8 +36,8 @@ const Banner = () => {
             {movie?.title || movie?.name || movie?.original_name}
         </h1>
         <div className="banner_buttons">
-            <button className="banner_button">Play</button>
-            <button className="banner_button play">My List</button>
+            <button className="banner_button play">Play</button>
+            <button className="banner_button ">My List</button>
         </div>
         <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
     </div>
